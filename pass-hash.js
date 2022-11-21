4ff3569d95f609e3e43940c4db8586ba113b2f9c
Juan Camilo <112782217+Donkami04@users.noreply.github.com>
2022-11-21 13:48:51 -0500

    Delete pass-hash.js

--- pass-hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function hashPassword(){
-  const myPassword = 'julantito#123';
-  const hash = await bcrypt.hash(myPassword, 10);
-  console.log(hash);
-};
-
-hashPassword();
